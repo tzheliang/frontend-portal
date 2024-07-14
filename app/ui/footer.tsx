8eec1448e39129970be840350c9eaf48a0b7303f
Zheliang Tan <zheliangtan@gmail.com>
2024-07-14 16:59:03 +0000

    update header, footer styles

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer({ footerText }: { footerText: string }) {
   return (
-    <div className="flex flex-row flex-auto w-full h-full p-4 justify-between bg-gray-100">
-      <p>{footerText}</p>
-      <p>Copyright &copy; 2024</p>
+    <div className="flex flex-row flex-auto w-full h-full p-4 justify-between bg-blue-700">
+      <p className="invert font-semibold">{footerText}</p>
+      <p className="invert font-semibold">Copyright &copy; 2024</p>
     </div>
   );
 }
--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -2,10 +2,11 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <div className="flex flex-row flex-auto w-full h-full bg-slate-400 rounded-b-md py-4 px-6">
+    <div className="flex flex-row flex-auto w-full h-full bg-blue-700 rounded-b-md py-4 px-6">
       <Image
         src="/next.svg"
         alt="Vercel Logo"
+        className='invert'
         width={100}
         height={24}
         priority
