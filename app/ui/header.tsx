4cac71ab9826f8ffd883b2c560f5e66800ce64dd
Zheliang Tan <zheliangtan@gmail.com>
2024-07-15 14:40:43 +0000

    fix header

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,16 +1,19 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div className="flex flex-row flex-auto w-full h-full bg-blue-700 rounded-b-md py-4 px-6">
-      <Image
-        src="/next.svg"
-        alt="Vercel Logo"
-        className='invert'
-        width={100}
-        height={24}
-        priority
-      />
+    <div className="flex flex-row flex-auto w-full h-full bg-blue-700 rounded-b-md py-4 px-6 items-center">
+      <Link href="/users">
+        <Image
+          src="/next.svg"
+          alt="Vercel Logo"
+          className="invert"
+          width={100}
+          height={24}
+          priority
+        />
+      </Link>
     </div>
   );
 }
